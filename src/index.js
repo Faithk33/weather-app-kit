81c3fedc30bbd0acb861ffa2a68737b73039cc84
Faithk33 <160004613+Faithk33@users.noreply.github.com>
2024-03-01 21:44:09 +0200

    added weather data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = response.data.temperature.current;
-  temperatureElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#city");
+  let humidityElement=document.querySelector("#humidity");
+  let windspeedElement=document.querySelector("#wind-speed");
+  let currentdateElement=document.querySelector("#current-date");
+  let date= new Date(response.data.time * 1000);
+
+  currentdateElement.innerHTML=formatDate(date);
+  windspeedElement.innerHTML='$(response.data.city.wind.speed)km/h';
+  humidityElement.innerHTML='$(response.data.city.temperature.humidity)%';
   cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML=Math.round(temperature);
 }
+function formatDate(date) {
+  let minutes= date.getMinutes();
+  let hour=date.getHours();
+  let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day=days[date.getDay()];
+
+  return '${day} ${hour}:${minutes}';
+}
+
 function displayCity(city) {
   let apiKey ="72bb9dab46b9ec3d65f423c63f27a9b8";
   let apiUrl =
